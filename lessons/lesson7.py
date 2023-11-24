49eb4512cb328ecf687f4caa0afc3666d3844833
Geek_Tech <geecktech.och2@gmail.com>
2023-11-24 18:20:54 +0600

    lesson7

--- lessons/lesson7.py
+++ lessons/lesson7.py
@@ -10,12 +10,27 @@ import sqlite3
 db = sqlite3.connect('op36_3.db')
 cursor = db.cursor()
 
-cursor.execute('''CREATE TABLE user(
+cursor.execute('''CREATE TABLE IF NOT EXISTS user (
 lastname TEXT,
 age INTEGER,
 view INTEGER,
 bitday DATE
 )''')
 
+# CREATE - INSERT INTO
+# cursor.execute('''INSERT INTO user VALUES ("beka",49,5,'2003-87-99')''')
 
+# UPDATE-UPDATE
+cursor.execute('''UPDATE user SET age=99 WHERE rowid!=2 ''')
+
+
+
+# REED-SELECT,fech
+cursor.execute('''SELECT rowid,* FROM user''')
+a=cursor.fetchall()
+for i in a:
+    print(i)
+
+
+db.commit()
 db.close()
